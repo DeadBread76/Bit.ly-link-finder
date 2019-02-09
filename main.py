b88a3da7983e5c2186ee98361cac19c3d92ae992
John <29266648+synchronocy@users.noreply.github.com>
2019-02-09 19:00:41 +0930

    Update main.py

--- main.py
+++ main.py
@@ -3,33 +3,25 @@ import random
 import string
 from requests_html import HTMLSession
 
-
-
 char_set = string.ascii_uppercase + string.digits + string.ascii_lowercase
+data = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'} # found more success using a user-agent
 
 while True:
     try:
         link = 'https://bit.ly/'+(''.join(random.sample(char_set*6, 6)))
-        src = requests.get(link)
-
+        src = requests.get(link, data=data)
         if src.status_code == 200:
-            print (link)
-            print (HTMLSession().get(link).html.find('title', first=True).text)
-            file = open("bitlylinks.txt","a")
-            file.write(str(link)+ '\n')
-            file.close()
-            file = open("bitlylinks.txt","a")
-            file.write((HTMLSession().get(link).html.find('title', first=True).text)+ '\n' + '\n')
-            file.close()
-
-        else:
-            print('Invalid link')
-
+            title = src.text
+            title = title[title.find('<title>')+7:title.find('</title>')]+'\n' # eh
+            with open("bitlylinks.txt","a") as handle:
+                try:
+                    if len(title) > 100: # check here incase the site spews its guts
+                        continue
+                    else:
+                        handle.write(title) 
+                        print(title)
+                    handle.write(str(link)+ '\n------------\n') #uh
+                except Exception:
+                    print('Error writing to disk.') # i mean I would return buuuuuuuuut
     except Exception:
-        print('Something fucked up there') 
-
-
-
-
-
-
+        print('Something fucked up, could not connect to bit.ly!\n')
